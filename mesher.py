3b142d9a93b8791cbb58d8a40c8da93ed33a51ea
Jonas Höchst <git@jonashoechst.de>
2016-08-26 15:28:01 +0200

    script for mesher-experimental evaluation, starting mesher and netmon service and preserving log files.

---
+++ mesher.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+from core import pycore
+from core import service
+from threading import Timer
+import datetime, time, shutil, os
+
+myservices_path = "/home/hoechst/.core/myservices"
+experiment_time_s = 5
+node_cnt = 10
+
+start_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M')
+logfolder = "/tmp/mesher-monitor-{}/".format(start_time)
+print("Started Experiment; log files will be copied to "+logfolder)
+
+print("### Creating CORE session.")
+session = pycore.Session(persistent=True)
+
+print("### Importing custom services from {}".format(myservices_path))
+service.CoreServices(session).importcustom(myservices_path)
+services = "DefaultRoute|NetmonService|MesherService"
+
+print("### Creating central network hub.")
+hub = session.addobj(cls=pycore.nodes.HubNode, name="hub")
+nodes = []
+
+def createCoreNode(i):
+    node = session.addobj(cls=pycore.nodes.CoreNode, name="n{}".format(i))
+    node.newnetif(hub, ["10.0.0.{}/24".format(i)])
+    session.services.addservicestonode(node, "", services, verbose=False)
+    return node
+    
+print("### Creating {} nodes, with services: {}".format(node_cnt, services))
+for i in range(node_cnt):
+    nodes.append(createCoreNode(i+1))
+
+def endExperiment():
+    print("\n### Ending Experiment; saving logfiles..."), 
+    os.mkdir(logfolder)
+    for f in os.listdir(session.sessiondir):
+        if f.endswith(".log"): shutil.move("{}/{}".format(session.sessiondir, f), logfolder)   
+    print("done.")
+    session.shutdown()
+
+
+print("\n### Scheduling Experiment ending in {}s.".format(experiment_time_s))
+t = Timer(experiment_time_s, endExperiment)
+t.start()
+
+print("### Starting node services..."),
+for n in nodes: service.CoreServices(session).bootnodeservices(n)
+print("done.")
+print("### Experiment is now running.")
+
