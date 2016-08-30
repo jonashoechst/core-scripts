bbd55304995f474c8d88f2a2c3af460238a4d66c
Jonas Höchst <git@jonashoechst.de>
2016-08-30 14:09:25 +0200

    major overwork:
     - netmon hub support
     - auto scheduler-copying
     - experiment naming

---
+++ helpers/netmon.py
@@ -0,0 +1,197 @@
+#!/usr/bin/python
+
+# Simple network monitor - 1s interval csv stats dumper
+# Copyright (c) 2016 Lars Baumgaertner, Jonas Hoechst
+#
+# requires dpkt and pcap python packages
+#
+# usage: sudo ./netmon.py <networkinterface>
+
+from __future__ import print_function
+import dpkt, pcap
+import signal
+import sys
+import time
+from threading import Thread
+
+total_cnt = {"pkt":0, "ip":0, "tcp":0, "udp":0, "tcp_port":0, "udp_port":0}
+total_size = {"pkt":0, "ip":0, "tcp":0, "udp":0,"tcp_port":0, "udp_port":0}
+
+cur_cnt = {"pkt":0, "ip":0, "tcp":0, "udp":0, "tcp_port":0, "udp_port":0}
+cur_size = {"pkt":0, "ip":0, "tcp":0, "udp":0, "tcp_port":0, "udp_port":0}
+
+def get_header():
+    return "timestamp_ms,cnt_pkt,cnt_ip,cnt_tcp,cnt_udp,cnt_tcp_port,cnt_udp_port,size_pkt,size_ip,size_tcp,size_udp,size_tcp_port,size_udp_port\n"
+
+def get_total_stats_human():
+    out = "\\n" , "="*40
+    out +="\n"+"Packet counts total:"
+    out +="\n"+ "# Pkts: ", total_cnt["pkt"]
+    out +="\n"+ "# IP: ", total_cnt["ip"]
+    out +="\n"+ "# tcp: ", total_cnt["tcp"]
+    out +="\n"+ "# udp: ", total_cnt["udp"]
+    out +="\n"+ "# tcp_port: ", total_cnt["tcp_port"]
+    out +="\n"+ "# udp_port: ", total_cnt["udp_port"]
+    out +="\n"+ "\\nPacket size counts total:"
+    out +="\n"+ "Pkts: ", total_size["pkt"]
+    out +="\n"+ "IP: ", total_size["ip"]
+    out +="\n"+ "tcp: ", total_size["tcp"]
+    out +="\n"+ "udp: ", total_size["udp"]
+    out +="\n"+ "tcp_port: ", total_size["tcp_port"]
+    out +="\n"+ "udp_port: ", total_size["udp_port"] + "\n"
+    return out
+
+def get_total_stats():
+    csv_line = "TOTAL,"
+    csv_line += "%d,%d,%d,%d,%d,%d" % (total_cnt["pkt"],total_cnt["ip"],total_cnt["tcp"],total_cnt["udp"],total_cnt["tcp_port"],total_cnt["udp_port"])
+    csv_line += ",%d,%d,%d,%d,%d,%d" % (total_size["pkt"],total_size["ip"],total_size["tcp"],total_size["udp"],total_size["tcp_port"],total_size["udp_port"])
+    csv_line += "\n"
+    return csv_line
+
+def get_cur_stats():
+    cur_time = int(time.time() * 1000)
+    csv_line = str(cur_time) + ","
+    csv_line += "%d,%d,%d,%d,%d,%d" % (cur_cnt["pkt"],cur_cnt["ip"],cur_cnt["tcp"],cur_cnt["udp"],cur_cnt["tcp_port"],cur_cnt["udp_port"])
+    csv_line += ",%d,%d,%d,%d,%d,%d" % (cur_size["pkt"],cur_size["ip"],cur_size["tcp"],cur_size["udp"],cur_size["tcp_port"],cur_size["udp_port"])
+    csv_line += "\n"
+
+    for i in cur_cnt.keys(): cur_cnt[i] = 0
+    for i in cur_size.keys(): cur_size[i] = 0
+
+    last_time = cur_time
+    return csv_line
+
+
+class LoggerThread(Thread):
+    def __init__(self, outpath=None):
+        Thread.__init__(self)
+        try:
+            if outpath: self.outfile = open(outpath, "w+")
+            else: self.outfile = None
+        except IOError:
+            sys.exit("Unable to write to file {}; using stdout".format(outpath))
+            self.outfile = None
+    
+    def write(self, msg):
+        if self.outfile: self.outfile.write(msg)
+        else: print(msg, end="") 
+    
+    def run(self):
+        self.write(get_header())
+        self.running = True
+        while self.running:
+            stats = get_cur_stats()
+            self.write(stats)
+            sys.stdout.flush()
+            time.sleep(1)
+        
+    def stop(self): self.running = False
+
+
+
+class PcapThread(Thread):
+    def __init__(self, interface, port=None):
+        Thread.__init__(self)
+        self.pc = pcap.pcap(name=interface)
+        self.last_time = time.time()
+        self.port = port
+        
+    def run(self):
+        self.running = True
+        while self.running: 
+            self.count_pkts()
+
+    def stop(self): self.running = False
+    
+    def count_pkts(self):
+        for timestamp, raw_buf in self.pc:
+            if not self.running: return
+            
+            try: self.count_pkt(raw_buf)
+            except Exception as e: 
+                print(e, file=sys.stderr)
+            
+    def count_pkt(self, raw_buf):
+        if not self.running: return
+        output = {}
+
+        # Unpack the Ethernet frame (mac src/dst, ethertype)
+        eth = dpkt.ethernet.Ethernet(raw_buf)
+        packet_size = len(raw_buf)
+        
+        global cur_cnt, total_cnt, cur_size, total_size
+        cur_cnt["pkt"] += 1
+        total_cnt["pkt"] += 1
+
+        cur_size["pkt"] += packet_size
+        total_size["pkt"] += packet_size
+
+        if eth.type != dpkt.ethernet.ETH_TYPE_IP: return
+        
+        ip = eth.data
+
+        cur_cnt["ip"] += 1
+        total_cnt["ip"] += 1
+
+        cur_size["ip"] += packet_size
+        total_size["ip"] += packet_size
+
+        if ip.p==dpkt.ip.IP_PROTO_TCP:
+           TCP=ip.data
+           cur_cnt["tcp"] += 1
+           total_cnt["tcp"] += 1
+           cur_size["tcp"] += packet_size
+           total_size["tcp"] += packet_size
+           if self.port != None and (TCP.dport == self.port or TCP.sport == self.port):
+               cur_cnt["tcp_port"] += 1
+               total_cnt["tcp_port"] += 1
+               cur_size["tcp_port"] += packet_size
+               total_size["tcp_port"] += packet_size
+
+        elif ip.p==dpkt.ip.IP_PROTO_UDP:
+           UDP=ip.data
+           cur_cnt["udp"] += 1
+           total_cnt["udp"] += 1
+           cur_size["udp"] += packet_size
+           total_size["udp"] += packet_size
+           if self.port != None and (UDP.dport == 4110 or UDP.sport == 4110):
+               cur_cnt["udp_port"] += 1
+               total_cnt["udp_port"] += 1
+               cur_size["udp_port"] += packet_size
+               total_size["udp_port"] += packet_size
+
+
+def start(interface, port=None, outpath=None):
+    """Start a netmon for the given interface name
+    
+    port: count spare traffic for this port
+    outpath: save netmon log to this port, instead of printing 
+    """
+    global pcapThread, loggerThread
+    pcapThread = PcapThread(interface)
+    loggerThread = LoggerThread(outpath=outpath)
+    pcapThread.start()
+    loggerThread.start()
+
+def stop():
+    """Stop the current netmon instance
+    """
+    global pcapThread, loggerThread
+    pcapThread.stop()
+    loggerThread.stop()
+    pcapThread.join()
+    loggerThread.join()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2: 
+        print("usage: {} <interface>".format(sys.argv[0]))
+        sys.exit(1)
+    start(sys.argv[1])
+    
+    def signal_handler(signal, frame):
+        stop()
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
+    
+    while True: time.sleep(60)
--- mesher.py
+++ mesher.py
@@ -2,17 +2,28 @@
 from core import pycore
 from core import service
 from threading import Timer
-import datetime, time, shutil, os, sys
+import datetime, time, shutil, os, sys, signal
+import helpers.netmon as netmon
 
 myservices_path = "/home/hoechst/.core/myservices"
-services = "DefaultRoute|NetmonService|MesherService"
+scheduler_targer = "/tmp/scheduler.js"
+services = "DefaultRoute|MesherService"
 
-def concieveLogfolder(description):
+def createLogfolder(description=None):
     start_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M')
-    logfolder = "/tmp/mesher-monitor/{}-{}/".format(description, start_time)
+    logfolder = "/tmp/mesher-monitor/"
+    if description: logfolder += "{}-".format(description)
+    logfolder += start_time
+    
+    if os.path.isdir(logfolder):
+        char = 97
+        while os.path.isdir("{}-{}".format(logfolder, chr(char))): char += 1
+        logfolder = "{}-{}".format(logfolder, chr(char))
+        
+    os.makedirs(logfolder)
     return logfolder
 
-def runMesherExperiment(duration, node_cnt, logfolder):
+def runMesherExperiment(duration, node_cnt, logfolder, scheduler=None):
     def createCoreNode(node_number, cable):
         if node_number < 1 or node_number > 254:
             raise Exception("0 < node_number < 256, since we use 10.0.0.0/24")
@@ -23,45 +34,92 @@ def runMesherExperiment(duration, node_cnt, logfolder):
 
     def endExperiment():
         print("\n### Ending experiment, saving logfiles..."),
-        os.makedirs(logfolder)
-        os.mkdir("{}/netmon".format(logfolder))
+        netmon.stop()
+        # os.mkdir("{}/netmon".format(logfolder))
         os.mkdir("{}/mesher".format(logfolder))
         for f in os.listdir(session.sessiondir):
             if f.endswith(".log"):
-                if f.startswith("netmon"): shutil.move("{}/{}".format(session.sessiondir, f), "{}/netmon".format(logfolder))
-                elif f.startswith("mesher"): shutil.move("{}/{}".format(session.sessiondir, f), "{}/mesher".format(logfolder))
+                # if f.startswith("netmon"): shutil.move("{}/{}".format(session.sessiondir, f), "{}/netmon".format(logfolder))
+                if f.startswith("mesher"): shutil.move("{}/{}".format(session.sessiondir, f), "{}/mesher".format(logfolder))
                 else: shutil.move("{}/{}".format(session.sessiondir, f), logfolder)
-        with open("{}configuration.csv".format(logfolder), "w+") as config:
-            config.write("duration: {}\n".format(duration))
+        with open("{}/configuration.csv".format(logfolder), "w+") as config:
+            config.write("duration, {}\n".format(duration))
             config.write("node_cnt, {}\n".format(node_cnt))
+            config.write("scheduler, {}\n".format(scheduler.split("/")[-1]))
         print("done.\n")
         session.shutdown()
+    
+    def copy_scheduler(scheduler):
+        if not scheduler:
+            print("No scheduler set, removing {}".format(scheduler_targer))
+        elif not os.path.exists(scheduler):
+            print("No file at \"{}\", removing {}".format(scheduler_targer))
+        else:
+            shutil.copyfile(scheduler, scheduler_targer)
+            shutil.copyfile(scheduler, "{}/scheduler.js".format(logfolder))
+            print("Scheduler successfully copied from \"{}\".".format(scheduler))
 
     print("Started Experiment; log files will be copied to "+logfolder)
+    copy_scheduler(scheduler)
+
     print("### Creating CORE session.")
     session = pycore.Session(persistent=True)
+    
+    def signal_handler(signal, frame):
+        endExperiment()
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
 
     print("### Importing custom services from {}".format(myservices_path))
     service.CoreServices(session).importcustom(myservices_path)
 
     print("### Creating central network hub.")
     hub = session.addobj(cls=pycore.nodes.HubNode, name="hub")
+    
+    print("### Attaching netmon to network hub.")
+    netmon.start(hub.brname, outpath="{}/netmon-hub.csv".format(logfolder), port=8032)
 
     print("### Creating {} nodes with services: {}".format(node_cnt, services))
     nodes = []
     for i in range(node_cnt):
         nodes.append(createCoreNode(i+1, hub))
 
-    print("\n### Scheduling experiment ending in {}s.".format(duration))
-    t = Timer(duration, endExperiment)
-    t.start()
-
     print("### Starting node services...")
     for n in nodes: service.CoreServices(session).bootnodeservices(n)
 
-    print("### Experiment is now running.")
-    t.join()
+    print("### Experiment is now running for {} seconds.\n".format(duration))
+    time.sleep(duration)
+    endExperiment()
 
 if __name__ == "__main__":
-    runMesherExperiment(2, 5, concieveLogfolder("testing5"))
-    runMesherExperiment(2, 10, concieveLogfolder("testing10"))
+    node_counts = [10] #[5, 10, 25, 50, 100]
+    durations = [10]
+    schedulers = []
+    
+    if len(sys.argv) != 2:
+        print("usage: {} [scheduler|scheduler-dir]".format(sys.argv[0]))
+        sys.exit(1)
+        
+    if os.path.isdir(sys.argv[1]):
+        for (_, _, filenames) in os.walk(sys.argv[1]):
+            for filename in filenames:
+                if filename.endswith('.js'): 
+                    schedulers.append(os.sep.join([sys.argv[1], filename]))
+    elif os.path.isfile(sys.argv[1]) and sys.argv[1].endswith('.js'):
+        schedulers.append(filename)
+    else:
+        print("No scheduler found at given path.\n")
+        print("usage: {} [scheduler|scheduler-dir]".format(sys.argv[0]))
+        sys.exit(2)
+    
+    for d in durations:
+        for n in node_counts:
+            for s in schedulers:
+                sname = s.split("/")[-1]
+                description = "{}-n{}".format(sname, n)
+                logfolder = createLogfolder(description)
+                runMesherExperiment(d, n, logfolder, scheduler=s)
+
+
+
+
