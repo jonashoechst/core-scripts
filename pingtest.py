4f226b21ceb0200ee5226e92cf6f486fe0dbc3ac
Jonas Höchst <git@jonashoechst.de>
2016-09-21 14:45:38 +0200

    two-clouded pingtest

--- pingtest.py
+++ pingtest.py
@@ -6,12 +6,15 @@ node_cnt = 10
 
 session = pycore.Session(persistent=True)
 
-hub = session.addobj(cls=pycore.nodes.HubNode, name="hub")
+hub1 = session.addobj(cls=pycore.nodes.HubNode, objid="a", name="a", verbose=True)
+hub2 = session.addobj(cls=pycore.nodes.HubNode, objid="b", name="b", verbose=True)
+hub1.linknet(hub2)
 nodes = []
 
 for i in range(node_cnt):
     node = session.addobj(cls=pycore.nodes.CoreNode, name="n{}".format(i))
-    node.newnetif(hub, ["10.0.0.{}/24".format(i)])
+    if i < node_cnt/2: node.newnetif(hub1, ["10.0.0.{}/24".format(i)])
+    else: node.newnetif(hub2, ["10.0.0.{}/24".format(i)])
     nodes.append(node)
     
 for n in nodes:
